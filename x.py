540c14c13d5a2529a230b55314fa6e6031caa0f0
Egor <c000299792458@gmail.com>
2024-10-02 02:15:46 +0300

    уничтожен

--- x.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber 
-
-
-with pdfplumber.open("test.pdf") as pdf:
-  text = pdf.pages[0]
-  clean_text = text.filter(lambda obj: obj['object_type'] == "char" and "Bold" in obj['fontname'])
-  print(clean_text.extract_text())
