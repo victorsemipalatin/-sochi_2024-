892ac5b73a38180104037b8e90d5365135cf5525
Мистер Картофель <126357702+JamradisePalms@users.noreply.github.com>
2024-10-01 22:09:57 +0300

    Финал №2

--- bert.py
+++ bert.py
@@ -1,31 +1,45 @@
 from transformers import BertTokenizer, BertForSequenceClassification, AutoTokenizer, AutoModel
 import torch
 import torch.nn as nn
+from transformers import pipeline
 from config import path_to_pt, path_to_txt
+import time
 
-
-def search_candidates(model: AutoModel, tokenizer: AutoTokenizer, threshold: float, device: str, path_to_txt: str) -> list[str]:
+def search_candidates(model: AutoModel, tokenizer: AutoTokenizer, batch_size, threshold: float, device: str, path_to_txt: str) -> list[str]:
     candidates = []
     with open(path_to_txt, 'r', encoding='UTF-8') as f:
-        line = f.readline()
-        rows = line.split("\n")
-        for row in rows:
-            inputs = tokenizer(row, max_length=32, truncation=True, padding=True, return_tensors='pt').to(device)
+        line = f.readlines()
+        rows = [row.strip().replace('_', ' ') for row in line if len(row.strip()) > 0]
+        for i in range(0, len(rows), batch_size):
+            batch = rows[i:i + batch_size]
+            inputs = tokenizer(batch, max_length=32, truncation=True, padding=True, return_tensors='pt').to(device)
             outputs = model(**inputs)
             predictions = torch.softmax(outputs.logits, dim=-1)
-            if predictions[0][1] > threshold:  # Можно менять порог вероятности заголовка.
-                candidates.append(tokenizer.decode(inputs['input_ids'][0].tolist(), skip_special_tokens=True))
+
+            for j, row in enumerate(batch):
+                phrase = tokenizer.decode(inputs['input_ids'][j].tolist(), skip_special_tokens=True)
+                if predictions[j][1] > threshold and len(phrase) > 2: # len(phrase) > 3 - КОСТЫЛЬ
+                    candidates.append(phrase)
+
+    for i in range(len(candidates)-1, 0, -1):
+        if candidates[i][0].lower() == candidates[i][0] and candidates[i][0].isalpha():
+            candidates[i-1] += ' ' + candidates[i]
+            candidates[i] = ''
+    candidates = [cand for cand in candidates if len(cand) > 0]
     return candidates
 
-
 if __name__ == "__main__":
-    threshold = 0.99
+    start_time = time.time()
+    threshold = 0.99 # <---- EXPERIMENT WITH THIS PARAMETER
+    batch_size = 64 # <---- CHANGE TO len(page)
     tokenizer = BertTokenizer.from_pretrained("DeepPavlov/rubert-base-cased-sentence", num_labels=2,
                                               output_attentions=False, output_hidden_states=False)
     model = BertForSequenceClassification.from_pretrained("DeepPavlov/rubert-base-cased-sentence")
     model.load_state_dict(torch.load(path_to_pt, weights_only=True))
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.to(device)
-
-    candidates = search_candidates(model, tokenizer, threshold, device, path_to_txt)
+    # TODO: ADD SPELL-CHECKER
+    with torch.no_grad():
+        candidates = search_candidates(model, tokenizer, batch_size, threshold, device, path_to_txt)
     print(candidates)
+    print('\n'.join(candidates))
